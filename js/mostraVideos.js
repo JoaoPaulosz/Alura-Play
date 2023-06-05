d4386f2f0a3307ab0850077e5f49033b540e7c9a
João Souza <joao.paulo.de.souza15t@gmail.com>
2023-06-04 22:21:37 -0300

    Aplicando a conexão com a API mockada

--- js/mostraVideos.js
+++ js/mostraVideos.js
@@ -1,27 +1,29 @@
-import { conectaApi } from "./conectaAPI"
+import { conectaApi } from "./conectaAPI.js"
 
-const list = document.querySelector("[data-lista]")
+const lista = document.querySelector("[data-lista]")
 
-function criaCadVideos(videos){
-    const video = document.createElement("li")
+function criaCadVideos(titulo, descricao, url, imagem){
+    const video = document.createElement('li')
     video.className = "video__item"
-    video.innerHTML += 
+    video.innerHTML = 
     `
-    <iframe width="100%" height="72%" src="${videos.url}"
-                title="YouTube video player" frameborder="0"
+    <iframe width="100%" height="72%" src="${url}"
+                title="${titulo}" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen></iframe>
     <div class="descricao-video">
-        <img src="./img/logo.png" alt="logo canal alura">
-        <h3>${videos.titulo}</h3>
-        <p>${videos.descricao}</p>
+        <img src="${imagem}" alt="logo canal alura">
+        <h3>${titulo}</h3>
+        <p>${descricao}</p>
     </div>
     `
+    return video
 }
 
 async function consomeApi(){
-    const listaVideos = await conectaApi.listaVideos;
-    listaVideos.forEach(video => {
-        criaCadVideos(video)
-    });
+    const listaVideos = await conectaApi.listaVideos();
+    listaVideos.forEach(video => lista.appendChild(
+        criaCadVideos(video.titulo, video.descricao, video.url, video.imagem)))
 }
+
+consomeApi()
